e165bbdef6de46802c4b47c3aa2f876581573a87
makaronshick <makaronshick@ukr.net>
2024-07-25 21:23:06 +0300

    fixed

--- script.js
+++ script.js
@@ -8,19 +8,23 @@ console.log(array); // Результат: [1, 3, 6, 2, 5, 7]
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
 
-const array = [4, 1, 3, 4, 6, 2, 5, 4, 7, 4];
+const array = [4, 1, 4, 3, 4, 6, 2, 4, 5, 4, 7, 4, 4];
 
 function removeElement(array, value) {
   for (const item of array) {
     
-    if (item === value) {
-      const index = array.indexOf(item);
-      array.splice(index, 1); 
+    if (array.includes(value)) {
+
+      const index = array.indexOf(value);
+      if (index !== -1) {
+
+        array.splice(index, 1);
+    }
     }
   }
 
   return array;
 };
-
+ debugger
 removeElement(array, 4);
 console.log(array); // Результат: [1, 3, 6, 2, 5, 7]
